7f7c7ce5b14a128e3ff839db915b3d50cf7c16e5
Mary Marks <maryleemarks@gmail.com>
2021-06-08 19:18:30 -0700

    add README
     Co-authored-by: Christine Murphy <csalonen89@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,11 @@ function add(number1, number2) {
   
   const number1 = parseInt(prompt("Enter a number:"));
   const number2 = parseInt(prompt("Enter another number:"));
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+  const result = multiply(number1, number2);
   
-  alert(add(number1, number2));
+  alert(result);
